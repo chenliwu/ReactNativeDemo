78845da58bd57b92a16495e8a81959e43e610988
chenliwu <30075213@qq.com>
2018-10-04 14:19:29 +0800

    TextInput组件练习

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ import BlinkApp from './src/StateDemo';
 import LotsOfStyle from './src/StyleDemo';
 import FixedDimensionsBasics from './src/FixedDimensionsDemo';
 import FlexDirectionBasics from './src/FlexboxDemo/FlexDirection';
-import JustifyContentBasics  from './src/FlexboxDemo/JustifyContent';
-import AlignItemsBasics  from './src/FlexboxDemo/AlignItems';
+import JustifyContentBasics from './src/FlexboxDemo/JustifyContent';
+import AlignItemsBasics from './src/FlexboxDemo/AlignItems';
 import FlexWrapBasics from './src/FlexboxDemo/FlexWrap';
 import FlexBasics from './src/FlexboxDemo/Flex';
 import AlignSelfBasics from './src/FlexboxDemo/AlignSelf';
 
 import ES6_Test from './src/ES6_Test/ES6_Test1';
 import ActivityIndicator_Test from './src/Component/ActivityIndicator_Test1';
+
+import TextInputBasics from './src/Component/TextInput/TextInputBasics';
 
 
 
@@ -29,4 +31,4 @@ import ActivityIndicator_Test from './src/Component/ActivityIndicator_Test1';
 //AppRegistry.registerComponent('ReactNativeDemo', () => JustifyContentBasics );
 //AppRegistry.registerComponent('ReactNativeDemo', () => AlignItemsBasics );
 
-AppRegistry.registerComponent('ReactNativeDemo', () => ActivityIndicator_Test );
+AppRegistry.registerComponent('ReactNativeDemo', () => TextInputBasics);
